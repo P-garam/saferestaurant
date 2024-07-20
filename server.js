f803686e6369790c28d47dc9260811e1837c484a
garam <ginny.us44@gmail.com>
2024-07-20 17:10:48 +0900

    v12

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 // server/server.js
 const express = require('express');
 const path = require('path');
-const apiRouter = require('./routes/api');
+const apiRouter = require('./server/routes/api');
 
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, './frontend')));
 app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+    res.sendFile(path.join(__dirname, './frontend/index.html'));
 });
 
 app.get('/busan', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/busan.html'));
+    res.sendFile(path.join(__dirname, './frontend/busan.html'));
 });
 
 app.get('/daegu', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/daegu.html'));
+    res.sendFile(path.join(__dirname, './frontend/daegu.html'));
 });
 
 app.listen(PORT, () => {
